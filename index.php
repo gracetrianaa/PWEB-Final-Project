c7983727ef8dcebeeb18ebc47f05c4e02ba2149e
gracetrianaa <gracetrianasurvinta@gmail.com>
2023-12-13 19:38:40 +0700

    web frontend

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNO Reserve</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css">
+    <!-- link to css-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <!-- header section -->
+    <header class="header fixed-top">
+        <div class="container">
+            <div class="align-items-center justify-content-between" style="display: flex; flex-wrap: wrap">
+                <a href="Home" class="logo">
+                    <img src="images/unoreserve.svg">
+                </a>
+                <nav class="nav">
+                    <a href="#Home">Home</a>
+                    <a href="#About">About</a>
+                    <a href="#Experience">Experience</a>
+                    <a href="#Reviews">Reviews</a>
+                    <a href="#Contact">Contact</a>
+                </nav>
+
+                <a href="#Contact" class="link-btn">Make Reservation</a>
+                <div id="menu-btn" class="fas fa-bars"></div>
+            </div>
+        </div>
+   
+    </header> 
+    <!-- header section ends -->
+
+    <!-- home section -->
+    <section class="home" id="home">
+        <div class="container">
+            <div class="row min-vh-100 align-items-center">
+                <div class="content">
+                    <h3>Serve you the Realness and Luxury of Coffee</h3>
+                    <p>Warm Greetings, <b>Coffee Aficionados!</b></p>
+                    <p>We choose and offer you our unequaled Coffee Beans from our own <b>FARM</b>. Make a <b>RESERVATION</b> now to taste a <b>LUXURY</b> in a Cup of Coffee.</p>
+                    <a href="#Contact" class="link-btn">Make Reservation</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- home section ends -->
+
+    <!-- about section -->
+    <section class="about" id="about">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6 image">
+                    <img src="images/about_img.png" id="page-image" class="w-100 mb-5 mb-md-0" alt="">
+                </div>
+
+                <div class="col-md-6 content">
+                    <span>About Us</span>
+                    <h3>#1 Coffee Beans Farm, Roastery, and Slow Bar</h3>
+                    <p>Uno RESERVE was established in 2000. Since then, we have been steadily producing TOP-GRADE numerous kinds of Coffee Beans to export accross the world and became the number UNO for our high-quality beans.
+                        Not only being Coffee Beans Producer, we also offer experience to you, Coffee Aficionados, to learn about Coffee Beans through our farm & roastery. On top of the experience, you can enjoy the Coffee Beans at our luxurious
+                        Slow Bar made and served by our Certified Coffee Master Barista.</p>
+                    <a href="#Contact" class="link-btn">Make Reservation</a>
+                 </div>
+            </div>
+        </div>
+    </section>
+    <!-- about section ends -->
+
+    <!-- experience section -->
+    <section class="experience" id="experience">
+        <h1 class="heading">Our Experience Package</h1>
+        <div class="box-container container">
+            <div class="box">
+                <img src="images/icon-1.svg" alt="">
+                <h3>Buy the Best Coffee Beans</h3>
+                <p>Come to Uno RESERVE to buy our High-Quality Beans that you can't find in other farm. We give you BEST DEALS!</p>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-2.svg" alt="">
+                <h3>Coffee Farm Tour</h3>
+                <p>We will take you to have a tour at our 1000 hectare coffee farm. You get to meet Lulu, Lala, and Lili, our adorable luwaks. Also, you're welcomed to taste every kind of beans we have. </p>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-3.svg" alt="">
+                <h3>See Uno RESERVE Roastery</h3>
+                <p>Are you curious how the coffee beans are processed? Let's take a look at the process to sort the best beans and produce premium coffee grounds at Uno RESERVE Roastery. </p>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-4.svg" alt="">
+                <h3>Enjoy Uno RESERVE Slow Bar</h3>
+                <p>Taste our artisan coffee which will be made by Our Certified Master Barista from 100% premium quality beans. Enjoy the uniqueness each beans bring, we guarantee you'll never forget the luxurious savor. Our Coffee Menu changes every season!</p>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-5.svg" alt="">
+                <h3>Learn How to Make Coffee</h3>
+                <p>Not enough being a coffee enjoyer? You can try a-day-course or even take a professional course at Uno RESERVE. You will learn from Our World Champion Master Barista with reasonable price.</p>
+            </div>
+
+            <div class="box">
+                <img src="images/icon-6.svg" alt="">
+                <h3>Group Package at Uno RESERVE</h3>
+                <p>With a group consists of minimum 10 person, you can get the whole experience (Farm Tour, Roastery, Slow Bar, Learn How to Make Coffee) with only Rp700.000/pax!</p>
+            </div>
+
+        </div>
+    </section>
+    <!-- experience section ends -->
+
+    <!-- Farm, Roastery, Slow Bar section -->
+    <section class="spaces">
+        <h1 class="heading">Uno RESERVE SPACE</h1>
+
+        <div class="box-container container">
+            <div class="box">
+                <img src="images/farm.svg" alt="">
+                <h3>Uno RESERVE Coffee Farm</h3>
+                <p>Our pride, 1000 ha coffee farm, with more than 10 varieties of beans</p>
+            </div>
+
+            <div class="box">
+                <img src="images/roastery.svg" alt="">
+                <h3>Uno RESERVE Roastery</h3>
+                <p>Place to produce our highest-quality beans</p>
+            </div>
+
+            <div class="box">
+                <img src="images/slow-bar.svg" alt="">
+                <h3>Uno RESERVE Slow Bar</h3>
+                <p>Our Sanctuary for you to luxuriate in</p>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Farm, Roastery, Slow Bar section ends -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" rel="stylesheet"></script>
+    <script src="js/script.js"></script>
+
+   
+</body>
+</html>
